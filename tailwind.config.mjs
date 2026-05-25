54f0f558b1fa16dd2351ec73d1fa0489b46d5424
Jorge10Borges <yulivic.ca@gmail.com>
2026-05-24 20:39:19 -0400

    Initial site setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	theme: {
+		extend: {
+			colors: {
+				sand: '#FDFBF7',
+				zen: '#2A7B88',
+				sunset: '#E27D60',
+				coral: '#D9534F',
+			},
+			fontFamily: {
+				display: ['"Playfair Display"', 'serif'],
+				body: ['"Plus Jakarta Sans"', 'sans-serif'],
+			},
+		},
+	},
+};
